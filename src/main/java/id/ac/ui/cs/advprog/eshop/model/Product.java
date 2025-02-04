080af1b2b77b55124b6d7b3a881b819b41b36102
rakbidb <rakbidb@gmail.com>
2025-02-05 01:12:22 +0700

    changed Product constructor (generates UUID for productid)

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,9 +3,11 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter @Setter
 public class Product {
-    private String productId;
+    private UUID productId;
     private String productName;
     private int productQuantity;
 }
