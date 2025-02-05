a227a5b5d5aceb3ad15ce0fdd62a0aa13387b871
rakbidb <rakbidb@gmail.com>
2025-02-05 14:06:15 +0700

    added methods needed for implement edit prod. feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -2,8 +2,12 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
+import java.util.UUID;
 
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
+    public Product findProductById(UUID id);
+    public int getProductIdx(Product product);
+    public Product editProduct(Product product);
 }
