8763922238b31c8b3d2fee1c7b54803383df4c91
rakbidb <rakbidb@gmail.com>
2025-02-28 19:05:50 +0700

    deleted System.out.println from editCarPost method on CarController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -44,9 +44,7 @@ public class CarController {
 
     @PostMapping("/editCar")
     public String editCarPost(@ModelAttribute Car car, Model model) {
-        System.out.println(car.getCarId());
         carService.update(car.getCarId(), car);
-
         return "redirect:listCar";
     }
 
