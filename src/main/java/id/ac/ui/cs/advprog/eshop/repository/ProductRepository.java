bef843ff877b164f4bc3f5dc38daeadb9dbd4c34
rakbidb <rakbidb@gmail.com>
2025-02-28 19:12:25 +0700

    modified ProductRepository.java to set Product ID

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -13,6 +13,10 @@ public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null) {
+            UUID uuid = UUID.randomUUID();
+            product.setProductId(uuid.toString());
+        }
         productData.add(product);
         return product;
     }
